142645f77f80470d51ad61f7ac638265b5eb5169
Łukasz Stolcman <v0lt.lucas@gmail.com>
2018-09-30 14:14:01 +0200

    details page

--- app/routes.py
+++ app/routes.py
@@ -33,6 +33,20 @@ def search_title(title, page):
     else:
         pages = range(1, math.ceil(int(data['totalResults'])/10)+1)
 
+    return render_template('search.html', data=data, title=title, error=error, pages=pages)
 
-    return render_template('search.html', data=data,title=title, error=error, pages=pages)
+
+@app.route('/details/<imdb_id>')
+def details(imdb_id):
+    error = None
+
+    fmt = 'http://www.omdbapi.com/?i={}&apikey={}'.format(imdb_id, app.config['OMDB_APIKEY'])
+    response = requests.get(fmt)
+    data = response.json()
+
+    if data['Response'] == 'False':
+        error = data['Error']
+
+    return render_template('details.html', data=data, error=error)
+
 
