b1a4b165c268f8c5acfae60549e1afb9f76e6b68
Łukasz Stolcman <v0lt.lucas@gmail.com>
2018-09-30 13:42:12 +0200

    url routing, paging support

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,9 @@
-from flask import render_template, flash, redirect
+from flask import render_template, flash, redirect, url_for
 from app import app
 from app.forms import SearchForm
 
 import requests
+import math
 
 
 @app.route('/')
@@ -12,16 +13,26 @@ def index():
 
 @app.route('/search', methods=['GET', 'POST'])
 def search():
-    data = None
-    error = None
     form = SearchForm()
     if form.validate_on_submit():
-        fmt = 'http://www.omdbapi.com/?s={}&apikey={}'.format(form.title.data, app.config['OMDB_APIKEY'])
-        response = requests.get(fmt)
-        data = response.json()
-        if data['Response'] == 'False':
-            error = data['Error']
-    return render_template('search.html', form=form, data=data, error=error)
+        return redirect(url_for('search') + '/' + form.title.data)
+    return render_template('search.html', form=form)
+
+@app.route('/search/<title>/<int:page>')
+@app.route('/search/<title>', defaults={'page':1})
+def search_title(title, page):
+    error = None
+    pages = None
+
+    fmt = 'http://www.omdbapi.com/?type=movie&s={}&page={}&apikey={}'.format(title, page, app.config['OMDB_APIKEY'])
+    response = requests.get(fmt)
+    data = response.json()
+
+    if data['Response'] == 'False':
+        error = data['Error']
+    else:
+        pages = range(1, math.ceil(int(data['totalResults'])/10)+1)
 
 
+    return render_template('search.html', data=data,title=title, error=error, pages=pages)
 
