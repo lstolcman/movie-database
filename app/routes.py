5412940183a524424db1bd720ade0cb4d7c3427f
Łukasz Stolcman <v0lt.lucas@gmail.com>
2018-09-30 12:33:58 +0200

    initial layout

--- app/routes.py
+++ app/routes.py
@@ -2,6 +2,8 @@ from flask import render_template, flash, redirect
 from app import app
 from app.forms import SearchForm
 
+import requests
+
 
 @app.route('/')
 def index():
@@ -11,8 +13,15 @@ def index():
 @app.route('/search', methods=['GET', 'POST'])
 def search():
     data = None
+    error = None
     form = SearchForm()
     if form.validate_on_submit():
-        data = {'title':'asd'}
-    return render_template('search.html', form=form, data=data)
+        fmt = 'http://www.omdbapi.com/?s={}&apikey={}'.format(form.title.data, app.config['OMDB_APIKEY'])
+        response = requests.get(fmt)
+        data = response.json()
+        if data['Response'] == 'False':
+            error = data['Error']
+    return render_template('search.html', form=form, data=data, error=error)
+
+
 
--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 
 class Config(object):
     SECRET_KEY = 'qwerty'
+    OMDB_APIKEY = 'f5ab3a5'
 
